68e2ec8ec246b9b37a531c0164734187980fc353
estan <elise.stankus@gmail.com>
2023-10-29 14:16:38 -0700

    added function to create todayWeather list

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,17 @@ fetch(queryURL)
     .then(function (response) {
        return response.json();
     })
+    .then(function (data) {
+        var todayTemp = document.createElement('li');
+        var todayWind = document.createElement('li');
+        var todayHumidity = document.createElement('li');
+        
+
+        todayTemp.textContent = 'Current temp: ' + data.main.temp + ' °F'
+        todayWind.textContent = 'Current wind: ' + data.wind.speed + ' miles/hour'
+        todayHumidity.textContent = 'Current humidity: ' + data.main.humidity + '%'
+
+        todayWeather.appendChild(todayTemp);
+        todayWeather.appendChild(todayWind);
+        todayWeather.appendChild(todayHumidity);
+})
