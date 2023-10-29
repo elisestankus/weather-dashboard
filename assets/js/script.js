928d7c584121474ce9f4dc64a447150d2d046756
estan <elise.stankus@gmail.com>
2023-10-29 16:34:16 -0700

    wrapped weatherQuery call in getWeather function, called getWeather in getLocation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var zipInput = document.querySelector("#zipcodeInput");
 var countryInput = document.querySelector('#countryInput');
 var submitLocation = document.querySelector('#submitLocation');
+var lat = 0;
+var lon = 0;
+
 
 function getLocation(event) {
     event.preventDefault();
@@ -14,36 +17,41 @@ function getLocation(event) {
             return response.json();
         })
         .then(function(data) {
+            lat = data.lat
+            lon = data.lon
             console.log(data.name)
             console.log(data.lat)
             console.log(data.lon)
+            console.log(lat)
+            console.log(lon)
+            getWeather();
         })
+        
 
 }
 
 submitLocation.addEventListener('click', getLocation)
-    
 
-//var todayWeather = document.querySelector("#todayWeather");
-//var lat = 37.91
-//var lon = -122.06
-//var queryURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat +'&lon=' + lon + '&appid=' + APIKey + '&units=imperial';
+var todayWeather = document.querySelector("#todayWeather");
 
-//fetch(queryURL)
-   // .then(function (response) {
-      // return response.json();
-    //})
-   // .then(function (data) {
-       // var todayTemp = document.createElement('li');
-        //var todayWind = document.createElement('li');
-      //  var todayHumidity = document.createElement('li');
+function getWeather() {
+    var weatherQueryURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat +'&lon=' + lon + '&appid=' + APIKey + '&units=imperial';
+    fetch(weatherQueryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var todayTemp = document.createElement('li');
+            var todayWind = document.createElement('li');
+            var todayHumidity = document.createElement('li');
         
 
-       // todayTemp.textContent = 'Current temp: ' + data.main.temp + ' °F'
-       // todayWind.textContent = 'Current wind: ' + data.wind.speed + ' miles/hour'
-      //  todayHumidity.textContent = 'Current humidity: ' + data.main.humidity + '%'
+            todayTemp.textContent = 'Current temp: ' + data.main.temp + ' °F'
+            todayWind.textContent = 'Current wind: ' + data.wind.speed + ' miles/hour'
+            todayHumidity.textContent = 'Current humidity: ' + data.main.humidity + '%'
 
-      //  todayWeather.appendChild(todayTemp);
-      //  todayWeather.appendChild(todayWind);
-     //   todayWeather.appendChild(todayHumidity);
-//})
+            todayWeather.appendChild(todayTemp);
+            todayWeather.appendChild(todayWind);
+            todayWeather.appendChild(todayHumidity);
+})
+}
