155bd7a759b3b7a503e8c5664238f67c86efda1d
estan <elise.stankus@gmail.com>
2023-10-29 15:41:22 -0700

    declared submitLocation var, wrapped geoQuery call in getLocation function, added submitLocation eventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,49 @@
 var zipInput = document.querySelector("#zipcodeInput");
 var countryInput = document.querySelector('#countryInput');
+var submitLocation = document.querySelector('#submitLocation');
 
-var geoQueryURL = 'http://api.openweathermap.org/geo/1.0/zip?zip=' + zipInput.value + ',' + countryInput.value + '&appid=' + APIkey
+function getLocation(event) {
+    event.preventDefault();
+    var zip = zipInput.value
+    var country = countryInput.value
+    console.log(zip)
+    console.log(country)
+    var geoQueryURL = 'http://api.openweathermap.org/geo/1.0/zip?zip=' + zip + ',' + country + '&appid=' + APIKey
+    fetch(geoQueryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data.name)
+            console.log(data.lat)
+            console.log(data.lon)
+        })
 
-fetch(geoQueryURL)
-    .then(function (response) {
-        return response.json();
-    })
+}
 
-var todayWeather = document.querySelector("#todayWeather");
-var lat = 37.91
-var lon = -122.06
-var queryURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat +'&lon=' + lon + '&appid=' + APIKey + '&units=imperial';
+submitLocation.addEventListener('click', getLocation)
+    
 
-fetch(queryURL)
-    .then(function (response) {
-       return response.json();
-    })
-    .then(function (data) {
-        var todayTemp = document.createElement('li');
-        var todayWind = document.createElement('li');
-        var todayHumidity = document.createElement('li');
+//var todayWeather = document.querySelector("#todayWeather");
+//var lat = 37.91
+//var lon = -122.06
+//var queryURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat +'&lon=' + lon + '&appid=' + APIKey + '&units=imperial';
+
+//fetch(queryURL)
+   // .then(function (response) {
+      // return response.json();
+    //})
+   // .then(function (data) {
+       // var todayTemp = document.createElement('li');
+        //var todayWind = document.createElement('li');
+      //  var todayHumidity = document.createElement('li');
         
 
-        todayTemp.textContent = 'Current temp: ' + data.main.temp + ' °F'
-        todayWind.textContent = 'Current wind: ' + data.wind.speed + ' miles/hour'
-        todayHumidity.textContent = 'Current humidity: ' + data.main.humidity + '%'
+       // todayTemp.textContent = 'Current temp: ' + data.main.temp + ' °F'
+       // todayWind.textContent = 'Current wind: ' + data.wind.speed + ' miles/hour'
+      //  todayHumidity.textContent = 'Current humidity: ' + data.main.humidity + '%'
 
-        todayWeather.appendChild(todayTemp);
-        todayWeather.appendChild(todayWind);
-        todayWeather.appendChild(todayHumidity);
-})
+      //  todayWeather.appendChild(todayTemp);
+      //  todayWeather.appendChild(todayWind);
+     //   todayWeather.appendChild(todayHumidity);
+//})
