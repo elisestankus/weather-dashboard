2074888a672ab172569b1daa9d9249759898555e
estan <elise.stankus@gmail.com>
2023-10-29 14:09:24 -0700

    declared todayWeather and queryURL vars

---
+++ assets/js/script.js
@@ -0,0 +1,2 @@
+var todayWeather = document.querySelector("#todayWeather");
+var queryURL = 'https://api.openweathermap.org/data/2.5/weather?lat=37.91&lon=-122.06&appid=' + APIKey + '&units=imperial';
