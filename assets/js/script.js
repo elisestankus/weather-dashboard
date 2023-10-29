19ea3fbaa4bb38ba1bd8dd702a65e7c0847198c0
estan <elise.stankus@gmail.com>
2023-10-29 14:14:40 -0700

    declared lat & lon vars, updated queryURL, and made API call using fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
 var todayWeather = document.querySelector("#todayWeather");
-var queryURL = 'https://api.openweathermap.org/data/2.5/weather?lat=37.91&lon=-122.06&appid=' + APIKey + '&units=imperial';
+var lat = 37.91
+var lon = -122.06
+var queryURL = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat +'&lon=' + lon + '&appid=' + APIKey + '&units=imperial';
+
+fetch(queryURL)
+    .then(function (response) {
+       return response.json();
+    })
